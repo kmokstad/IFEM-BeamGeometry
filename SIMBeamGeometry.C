496634576565d087372b7c02b524f18bb5d49cc1
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2024-03-18 17:42:25 +0100

    Add calculation of deformations based on interpolated beam solution

--- SIMBeamGeometry.C
+++ SIMBeamGeometry.C
@@ -13,7 +13,10 @@
 
 #include "SIMBeamGeometry.h"
 #include "readTesselation.h"
+#include "ASMs1D.h"
+#include "IntegrandBase.h"
 #include "Utilities.h"
+#include "Vec3Oper.h"
 #include "VTF.h"
 #include "IFEM.h"
 #include "tinyxml2.h"
@@ -81,4 +84,162 @@ bool SIMBeamGeometry::writeGlvG (int& nBlock, const char* inpFile, bool doClear)
                << myGeometry->getNoNodes() <<")"<< std::endl;
 
   return this->getVTF()->writeGrid(myGeometry,"Tesselated geometry",++nBlock);
+}
+
+
+bool SIMBeamGeometry::getBeamSolution (const ASMs1D& patch, const Vec3& X,
+                                       Vec3& U, Tensor& T) const
+{
+  // Find element containing this point
+  std::pair<int,double> elm = patch.findElement(X);
+  if (elm.first < 1) return false; // Point is not in this patch
+
+  // Extract the element displacement vector
+  Vectors eV;
+  if (!myProblem->initElement1(*(patch.begin_elm()+elm.first-1),eV))
+    return false;
+  else if (eV.front().size() != 12)
+  {
+    // Consider support for higher-order elements later...
+    std::cerr <<" *** SIMBeamGeometry::getBeamSolution: For linear element only"
+              << std::endl;
+    return false;
+  }
+
+  // Interpolate using first-order Lagrange polynomials.
+  // TODO: Use Hermitian interpolation.
+  auto&& N1 = [](double xi) { return 0.5*(1.0 - xi); };
+  auto&& N2 = [](double xi) { return 0.5*(1.0 + xi); };
+
+  Vec3 R;
+  for (int i = 0; i < 3; i++)
+  {
+    U[i] = N1(elm.second)*eV.front()[i]   + N2(elm.second)*eV.front()[6+i];
+    R[i] = N1(elm.second)*eV.front()[3+i] + N2(elm.second)*eV.front()[9+i];
+  }
+  T = Tensor(R.x,R.y,R.z);
+
+  // TODO: Transform from local to global (assumed identity transform for now)
+  return true;
+}
+
+
+int SIMBeamGeometry::writeGlvS1 (const Vector& psol, int iStep, int& nBlock,
+                                 double, const char* pvecName, int idBlock,
+                                 int psolComps, bool)
+{
+  VTF* vtf = this->getVTF();
+  if (psol.empty() || !vtf)
+    return 0; // no primary solution
+
+  IntVec vID;
+  std::vector<IntVec> sID;
+  sID.reserve(this->getNoFields());
+
+  Matrix field;
+  Vector lovec;
+
+  int geomID = this->getStartGeo();
+  for (const ASMbase* pch : myModel)
+    if (!pch->empty())
+    {
+      if (msgLevel > 1)
+        IFEM::cout <<"Writing primary solution for patch "
+                   << pch->idx+1 << std::endl;
+
+      // Evaluate primary solution variables
+      pch->extractNodeVec(psol,lovec);
+      if (!pch->evalSolution(field,lovec,opt.nViz))
+        return -1;
+
+      // Output as vector field
+      if (!vtf->writeVres(field,++nBlock,++geomID,nsd))
+        return -2;
+      else
+        vID.push_back(nBlock);
+
+      // Output as scalar fields
+      for (size_t j = 1; j <= field.rows(); j++)
+        if (!vtf->writeNres(field.getRow(j),++nBlock,geomID))
+          return -3;
+        else if (j > sID.size())
+          sID.push_back({nBlock});
+        else
+          sID[j-1].push_back(nBlock);
+    }
+
+  if (myGeometry && myProblem)
+  {
+    if (msgLevel > 1)
+      IFEM::cout <<"Writing deformation for tesselated geometry"<< std::endl;
+
+    Vec3 Xb, Ub, Upt;
+    Tensor Tb(nsd);
+    field.resize(nsd,myGeometry->getNoNodes());
+    const ASMs1D* myPatch = nullptr;
+    for (const ASMbase* pch : myModel)
+      if (!pch->empty() && (myPatch = dynamic_cast<const ASMs1D*>(pch)))
+      {
+        myPatch->extractNodeVec(psol,myProblem->getSolution());
+        for (size_t i = 0; i < field.cols(); i++)
+        {
+          const Vec3& X0 = myGeometry->getCoord(i);
+
+          // Assume for now the beam is aligned with the global X-axis.
+          // TODO: Project X0 onto the beam model, account for multi-patch, etc.
+          Xb.x = X0.x;
+
+          // Calculate deformation at this point from the beam solution
+          if (this->getBeamSolution(*myPatch,Xb,Ub,Tb))
+          {
+            Upt = (Xb+Ub) + Tb*(X0-Xb) - X0;
+            field.fillColumn(1+i,Upt.ptr());
+          }
+        }
+      }
+
+    // Output as vector field
+    if (!vtf->writeVres(field,++nBlock,++geomID,nsd))
+      return -2;
+    else
+      vID.push_back(nBlock);
+
+    // Output as scalar fields
+    for (size_t j = 1; j <= field.rows(); j++)
+      if (!vtf->writeNres(field.getRow(j),++nBlock,geomID))
+        return -3;
+      else
+        sID[j-1].push_back(nBlock);
+  }
+
+  // Write result block identifications
+
+  bool ok = true;
+  if (!vID.empty())
+  {
+    if (pvecName)
+      ok = vtf->writeVblk(vID,pvecName,idBlock,iStep);
+    else
+      ok = vtf->writeDblk(vID,"Solution",idBlock,iStep);
+  }
+
+  if (idBlock <= static_cast<int>(nsd))
+    idBlock = nsd+1; // since we might have written BCs
+
+  std::string pname(pvecName ? pvecName : "d"); pname += "_w";
+  for (size_t i = 0; i < sID.size() && !sID[i].empty() && ok; i++)
+  {
+    if (myProblem && (!pvecName || psolComps <= 0))
+      pname = myProblem->getField1Name(i);
+    else if (i > 0 && i%nsd == 0)
+    {
+      pname.back() = 'r';
+      pname += 'x';
+    }
+    else
+      ++pname.back();
+    ok = vtf->writeSblk(sID[i],pname.c_str(),idBlock++,iStep);
+  }
+
+  return ok ? idBlock : -4;
 }
--- SIMBeamGeometry.h
+++ SIMBeamGeometry.h
@@ -17,6 +17,8 @@
 #include "SIMElasticBar.h"
 #include "ElementBlock.h"
 
+class ASMs1D;
+
 
 /*!
   \brief Driver class for beams with separate geometry for visualization.
@@ -26,7 +28,8 @@ class SIMBeamGeometry : public SIMElasticBar
 {
 public:
   //! \brief The constructor forwards to the parent class constructor.
-  explicit SIMBeamGeometry(const char* hd) : SIMElasticBar(hd) {}
+  explicit SIMBeamGeometry(const char* hd, unsigned char n = 1)
+    : SIMElasticBar(hd,n) {}
   //! \brief Empty destructor.
   virtual ~SIMBeamGeometry() {}
 
@@ -35,12 +38,33 @@ public:
   //! the tesselated geometry of the beam.
   virtual bool writeGlvG(int& nBlock, const char* inpFile, bool doClear);
 
+  //! \brief Writes primary solution for a given load/time step to the VTF-file.
+  //! \param[in] psol Primary solution vector
+  //! \param[in] iStep Load/time step identifier
+  //! \param nBlock Running result block counter
+  //! \param[in] pvecName Optional name of the primary vector field solution
+  //! \param[in] idBlock Starting value of result block numbering
+  //! \param[in] psolCmps Optional number of primary solution components
+  //!
+  //! \details This method is overridden to also write out the deformation
+  //! on the tesselated geometry based on interpolation of the beam solution.
+  virtual int writeGlvS1(const Vector& psol, int iStep, int& nBlock, double,
+                         const char* pvecName, int idBlock, int psolCmps, bool);
+
 protected:
   using SIMElasticBar::parse;
   //! \brief Parses a data section from an XML element.
   //! \details This method is overridden to also read in
   //! the tesselated geometry used for visualizing the beam.
   virtual bool parse(const tinyxml2::XMLElement* elem);
+
+  //! \brief Evaluates the interpolated beam solution at a specified point.
+  //! \param[in] patch The patch containing the point to evaluate at
+  //! \param[in] X Spatial coordinates of the point to evaluate at
+  //! \param[out] U Global displacements at the point
+  //! \param[out] T Global rotation matrix at the point
+  bool getBeamSolution(const ASMs1D& patch, const Vec3& X,
+                       Vec3& U, Tensor& T) const;
 
 private:
   ElementBlock* myGeometry = nullptr; //!< Tesselated geometry
